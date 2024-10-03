15e28069038c33ce025bcef533359eb03cb578b2
wtf2408 <bogdanevseyv@yandex.ru>
2024-10-03 11:54:31 +0300

    закончил первую задачу

---
+++ kasd_labs_winforms/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace kasd_labs_winforms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.vectorLabel = new System.Windows.Forms.Label();
+            this.labelMatrix = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(271, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // vectorLabel
+            // 
+            this.vectorLabel.AutoSize = true;
+            this.vectorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.vectorLabel.Location = new System.Drawing.Point(115, 72);
+            this.vectorLabel.Name = "vectorLabel";
+            this.vectorLabel.Size = new System.Drawing.Size(92, 32);
+            this.vectorLabel.TabIndex = 1;
+            this.vectorLabel.Text = "label2";
+            // 
+            // labelMatrix
+            // 
+            this.labelMatrix.AutoSize = true;
+            this.labelMatrix.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelMatrix.Location = new System.Drawing.Point(540, 72);
+            this.labelMatrix.Name = "labelMatrix";
+            this.labelMatrix.Size = new System.Drawing.Size(92, 32);
+            this.labelMatrix.TabIndex = 2;
+            this.labelMatrix.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelMatrix);
+            this.Controls.Add(this.vectorLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label vectorLabel;
+        private System.Windows.Forms.Label labelMatrix;
+    }
+}
+
---
+++ kasd_labs_winforms/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace kasd_labs_winforms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string matrixView = "Matrix:\n";
+            string vectorView = "vector:\n";
+
+            string[] lines = File.ReadAllLines("D:\\KUBGU\\kasd-labs\\kasd_labs_winforms\\cond.txt");
+            int n = int.Parse(lines[0]);
+
+            double[,] G = new double[n, n];
+            double[] x = new double[n];
+
+            // Заполнение матрицы G
+            for (int i = 0; i < n; i++)
+            {
+                matrixView += "(";
+                string[] line = lines[i + 1].Split();
+                for (int j = 0; j < n; j++)
+                {
+                    matrixView += $" {line[j]} ";
+                    G[i, j] = double.Parse(line[j]);
+                }
+                matrixView += ")\n";
+            }
+            // Заполнение вектора x
+            string[] xLine = lines[n + 1].Split();
+            vectorView += "(";
+            for (int i = 0; i < n; i++)
+            {
+                vectorView += $" {xLine[i]} ";
+                x[i] = double.Parse(xLine[i]);
+            }
+            vectorView += ")\n";
+
+            // Проверка на симметричность матрицы G
+            if (!IsSymmetric(G, n))
+            {
+                MessageBox.Show("Матрица G не является симметричной.");
+                return;
+            }
+
+            // Вычисление длины вектора
+            double length = CalculateLength(x, G, n);
+
+            labelMatrix.Text = matrixView;
+            vectorLabel.Text = vectorView;
+            label1.Text = $"Длина вектора: {Math.Floor(length)}";
+        }
+
+        static bool IsSymmetric(double[,] matrix, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] != matrix[j, i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        static double CalculateLength(double[] x, double[,] G, int n)
+        {
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    sum += x[i] * G[i, j] * x[j];
+                }
+            }
+            return Math.Sqrt(Math.Floor(sum));
+        }
+    }
+}
