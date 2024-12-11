a9078096b389f1fefdf97da6caa3cfc8230625ca
wtf2408 <bogdanevseyv@yandex.ru>
2024-12-11 11:56:26 +0300

    added task12

--- kasd_labs_console/Program.cs
+++ kasd_labs_console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
@@ -13,31 +14,87 @@ namespace kasd_labs_console
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            int[] mass = { 7, 3, 9, 2, 0, 18 };
-            MyPriorityQueue<int> q = new MyPriorityQueue<int>(mass);
-            q.Element();
-            for (int i = 0; i < q.Size; i++) { Console.Write(q[i] + " "); }
-            Console.WriteLine();
+            static void Main(string[] args)
+            {
 
-            Console.WriteLine(q.Poll());
-            for (int i = 0; i < q.Size; i++) { Console.Write(q[i] + " "); }
-            Console.WriteLine();
+                string file = "../../log.txt";
+                try
+                {
+                    StreamWriter streamWriter = new StreamWriter(file);
+                    Console.WriteLine("Enter n:");
+                    int n = int.Parse(Console.ReadLine());
 
-            int[] mass2 = { 9, 3, 8, 2, 19 };
-            MyPriorityQueue<int> q2 = new MyPriorityQueue<int>(mass2);
-            MyPriorityQueue<int> q3 = new MyPriorityQueue<int>(q2);
-            for (int i = 0; i < q.Size; i++) { Console.Write(q3[i] + " "); }
-            Console.WriteLine();
 
-            Console.WriteLine(q3.Poll());
-            for (int i = 0; i < q.Size; i++) { Console.Write(q3[i] + " "); }
-            Console.WriteLine();
+                    MyPriorityQueue<Request> queue = new MyPriorityQueue<Request>();
 
-            Console.ReadLine();
 
+                    Random random = new Random();
+                    Stopwatch stopwatch = new Stopwatch();
+                    RequestInfo[] times = new RequestInfo[n * 10 + 1];
+
+                    Request req, lastReq;
+                    RequestInfo time;
+                    int requestsCount, number = 1;
+
+
+                    stopwatch.Start();
+                    for (int i = 1; i <= n; i++)
+                    {
+                        requestsCount = random.Next(1, 11);
+                        for (int j = 1; j <= requestsCount; j++)
+                        {
+                            req = new Request(random.Next(1, 6), number, i);
+                            queue.Add(req);
+                            time = new RequestInfo(stopwatch.Elapsed.TotalMilliseconds, req.priority, req.number, req.step);
+                            times[number] = time;
+                            streamWriter.Write($"add {req.number} {req.priority} {req.step}\n");
+                            number++;
+                        }
+
+
+                        lastReq = queue.Poll();
+                        double waitingTime = stopwatch.Elapsed.TotalMilliseconds - times[lastReq.number].time;
+                        time = new RequestInfo(waitingTime, lastReq.priority, lastReq.number, lastReq.step);
+                        times[lastReq.number] = time;
+                        streamWriter.Write($"remove {lastReq.number} {lastReq.priority} {lastReq.step}\n");
+                    }
+
+                    while (!queue.IsEmpty())
+                    {
+                        lastReq = queue.Poll();
+                        double waitingTime = stopwatch.Elapsed.TotalMilliseconds - times[lastReq.number].time;
+                        time = new RequestInfo(waitingTime, lastReq.priority, lastReq.number, lastReq.step);
+                        times[lastReq.number] = time;
+                        streamWriter.Write($"remove " + $"{lastReq.number} " + $"{lastReq.priority} " + $"{lastReq.step}\n");
+                    }
+                    stopwatch.Stop();
+
+                    int maxIndex = 1;
+                    for (int i = 2; i < times.Length; i++)
+                    {
+                        if (times[i].time > times[maxIndex].time) { maxIndex = i; }
+                    }
+
+                    Console.Write("max time :" + times[maxIndex].time);
+                    Console.WriteLine();
+                    Console.Write("num : " + times[maxIndex].number);
+                    Console.WriteLine();
+                    Console.Write("priority : " + times[maxIndex].priority);
+                    Console.WriteLine();
+                    Console.Write("iteration :" + times[maxIndex].step);
+                    Console.WriteLine();
+
+                    streamWriter.Close();
+
+                    Console.ReadLine();
+                }
+                catch (Exception exep) { Console.WriteLine(exep.ToString()); }
+            }
+            static int Comparison(Request request1, Request request2)
+            {
+                return request1.priority - request2.priority;
+            }
         }
-        
+
     }
-}
+        
---
+++ kasd_labs_console/Request.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace kasd_labs_console
+{
+    struct Request : IComparable<Request>
+    {
+        public int priority;
+        public int number;
+        public int step;
+        public Request(int priority, int number, int step)
+        {
+            this.priority = priority;
+            this.number = number;
+            this.step = step;
+        }
+        public int CompareTo(Request req)
+        {
+            return priority - req.priority;
+        }
+    }
+}
