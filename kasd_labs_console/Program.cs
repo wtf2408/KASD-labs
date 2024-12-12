33c0d0a1cd2a491818b8c872641246d8a46b865a
wtf2408 <bogdanevseyv@yandex.ru>
2024-12-12 16:29:14 +0300

    added task14

--- kasd_labs_console/Program.cs
+++ kasd_labs_console/Program.cs
@@ -14,87 +14,21 @@ namespace kasd_labs_console
 {
     internal class Program
     {
-            static void Main(string[] args)
-            {
+        static void Main(string[] args)
+        {
+            var deque = new MyArrayDeque<int>();
+            int[] plainArray = Enumerable.Range(1, 20).ToArray();
 
-                string file = "../../log.txt";
-                try
-                {
-                    StreamWriter streamWriter = new StreamWriter(file);
-                    Console.WriteLine("Enter n:");
-                    int n = int.Parse(Console.ReadLine());
+            deque.AddAll(plainArray);
+            deque.Add(21);
+            deque.Add(22);
+            Console.WriteLine($"Первый эл.: {deque.GetFirst()}");
+            Console.WriteLine($"Последний эл.: {deque.GetLast()}");
 
-
-                    MyPriorityQueue<Request> queue = new MyPriorityQueue<Request>();
-
-
-                    Random random = new Random();
-                    Stopwatch stopwatch = new Stopwatch();
-                    RequestInfo[] times = new RequestInfo[n * 10 + 1];
-
-                    Request req, lastReq;
-                    RequestInfo time;
-                    int requestsCount, number = 1;
-
-
-                    stopwatch.Start();
-                    for (int i = 1; i <= n; i++)
-                    {
-                        requestsCount = random.Next(1, 11);
-                        for (int j = 1; j <= requestsCount; j++)
-                        {
-                            req = new Request(random.Next(1, 6), number, i);
-                            queue.Add(req);
-                            time = new RequestInfo(stopwatch.Elapsed.TotalMilliseconds, req.priority, req.number, req.step);
-                            times[number] = time;
-                            streamWriter.Write($"add {req.number} {req.priority} {req.step}\n");
-                            number++;
-                        }
-
-
-                        lastReq = queue.Poll();
-                        double waitingTime = stopwatch.Elapsed.TotalMilliseconds - times[lastReq.number].time;
-                        time = new RequestInfo(waitingTime, lastReq.priority, lastReq.number, lastReq.step);
-                        times[lastReq.number] = time;
-                        streamWriter.Write($"remove {lastReq.number} {lastReq.priority} {lastReq.step}\n");
-                    }
-
-                    while (!queue.IsEmpty())
-                    {
-                        lastReq = queue.Poll();
-                        double waitingTime = stopwatch.Elapsed.TotalMilliseconds - times[lastReq.number].time;
-                        time = new RequestInfo(waitingTime, lastReq.priority, lastReq.number, lastReq.step);
-                        times[lastReq.number] = time;
-                        streamWriter.Write($"remove " + $"{lastReq.number} " + $"{lastReq.priority} " + $"{lastReq.step}\n");
-                    }
-                    stopwatch.Stop();
-
-                    int maxIndex = 1;
-                    for (int i = 2; i < times.Length; i++)
-                    {
-                        if (times[i].time > times[maxIndex].time) { maxIndex = i; }
-                    }
-
-                    Console.Write("max time :" + times[maxIndex].time);
-                    Console.WriteLine();
-                    Console.Write("num : " + times[maxIndex].number);
-                    Console.WriteLine();
-                    Console.Write("priority : " + times[maxIndex].priority);
-                    Console.WriteLine();
-                    Console.Write("iteration :" + times[maxIndex].step);
-                    Console.WriteLine();
-
-                    streamWriter.Close();
-
-                    Console.ReadLine();
-                }
-                catch (Exception exep) { Console.WriteLine(exep.ToString()); }
-            }
-            static int Comparison(Request request1, Request request2)
-            {
-                return request1.priority - request2.priority;
-            }
+            Console.WriteLine($"Попный эл.: {deque.Pop()}");
+            Console.WriteLine($"Первый эл.: {deque.GetFirst()}");
+            Console.WriteLine($"Последний эл.: {deque.GetLast()}");
         }
-
     }
+}
         
